5cf45a842a7869c24e82a5657c05d96378b78c2f
Luke Logan <llogan382@gmail.com>
2019-08-30 12:54:54 -0400

    Almost works

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ Allow users to get info from the census for their city, and compare with state a
 // From a dropdown, select the CATEGORY of info to search for
 // Select GEOGRAPHIC AREA
 
+const gridSelector = document.querySelector('.gridContainer');
 const stateSelector = document.getElementById('stateDropdown');
 const tagSelector = document.getElementById('tagDropdown');
 const countySelector = document.getElementById('countyDropdown');
@@ -34,12 +35,7 @@ const res = fetch(
     }
   });
 
-/* eslint-disable */
-// countySelector.addEventListener("change", () => {
-//   queryValue = tagSelector.value;
-//   getTags(queryValue);
-//   // return queryValue;
-// });
+
 
 stateSelector.addEventListener("change", getCounties);
 
@@ -77,7 +73,7 @@ function getTags() {
 
 
       // Create an array and push items to end of the array
-      for (var i = 0; i < 20; i++) {
+      for (var i = 0; i < 10; i++) {
         randValues.push(allKeys[Math.floor(Math.random() * allKeys.length)]);
       }
       for (var values of randValues) {
@@ -88,7 +84,6 @@ function getTags() {
     )
   );
 }
-getTags();
 
 
 function mainQuery(randVars) {
@@ -97,47 +92,32 @@ function mainQuery(randVars) {
     queryVars += "," + censusKeys[0][0]
   }
   const county = fetch(`
-  https://api.census.gov/data/2017/acs/acs1/subject?get=NAME${queryVars}&for=county:081&in=state:37&key=${key}`).then(
-    county =>
-      county.json().then(county => {
-        // County will have all county info
-        // ItemName hase the value
-        for (const [i, itemName] of randVars.entries()) {
-          countyDiv.innerHTML += `${itemName[0][1].label}</br>
-          ${county[1][i + 1]} </br>
-          `;
-          // console.log(itemName[0][1].label, county[1][i + 1]);
-        }
-      })
-  );
-  // const county = fetch(`
-  //   https://api.census.gov/data/2017/acs/acs1/subject?get=NAME${rand}&for=county:${countySelector.value}&in=state:${stateSelector.value}&key=${key}`).then(
-  //   county =>
-  //     county.json().then(county => {
-  //       console.table(county);
-  //     })
-  // );
-}
-// https://api.census.gov/data/2017/acs/acs1/subject?get=NAME,S0101_C01_001E&for=county:013&in=state:04&key=YOUR_KEY_GOES_HERE
-// const state = fetch(
-//   `https://api.census.gov/data/2017/acs/acs1/profile?get=NAME,${variableSelector.value}&for=state:${stateSelector.value}&key=${key}`
-// );
-// const country = fetch(
-//   `https://api.census.gov/data/2017/acs/acs1/profile?get=NAME,${variableSelector.value}&for=us:1&key=${key}`
-// );
-// Promise.all([county, state, country])
-// Transform promise with .then, which is unformatted
-// .then(
-//   responses => {
-//     return Promise.all(responses.map(res => res.json()));
-//   }
-// Call it in json, which will return a promise
-// answer.json()
-// )
-// .then(responses => {
-//   console.log(responses);
-//   countyDiv.innerHTML += `Value: ${responses[0][1][1]}`;
-//   stateDiv.innerHTML += `Value: ${responses[1][1][1]}`;
-//   countryDiv.innerHTML += `Value: ${responses[2][1][1]}`;
-// });
+    https://api.census.gov/data/2017/acs/acs1/subject?get=NAME${queryVars}&for=county:${countySelector.value}&in=state:${stateSelector.value}&key=${key}`);
 
+  const state = fetch(
+    `https://api.census.gov/data/2017/acs/acs1/subject?get=NAME${queryVars}&for=state:${stateSelector.value}&key=${key}`
+  );
+  const country = fetch(
+    `https://api.census.gov/data/2017/acs/acs1/subject?get=NAME${queryVars}&for=us:1&key=${key}`
+  );
+  Promise.all([county, state, country])
+    // Transform promise with .then, which is unformatted
+    .then(
+      responses => {
+        return Promise.all(responses.map(res => res.json()))
+      }
+      // Call it in json, which will return a promise
+      // answer.json()
+    )
+    .then(res => {
+      // County will have all county info
+      // ItemName hase the value
+      for (const [i, itemName] of randVars.entries()) {
+        console.log(res, itemName);
+        // gridSelector.innerHTML += `<div class="gridItem">County: ${itemName[0][1].label}</br>
+        //   ${entries[1][i + 1]} </br>
+        //   </div>
+        //       `
+      }
+    });
+}
